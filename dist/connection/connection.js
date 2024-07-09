269af470ef5b33f9d06eb3657d33a3769eb4f4cb
NIitishKumar <nitishkumar.ng746@gmail.com>
2024-07-09 16:48:57 +0530

    node to nest

--- dist/connection/connection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('tracker', 'root', 'Nitish@1234', {
-    dialect: 'mysql', // Set the database dialect to MySQL
-    host: 'localhost', // Change to your MySQL database host
-});
-exports.default = sequelize;
-//# sourceMappingURL=connection.js.map
--- src/connection/connection.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('tracker', 'root', 'Nitish@1234', {
-  dialect: 'mysql', // Set the database dialect to MySQL
-  host: 'localhost', // Change to your MySQL database host
-});
-
-export default sequelize;
